1cf499921afd778a7b83949f946c87ac9f630b36
Oleh Malakan <olehdevt@gmail.com>
2023-11-05 15:10:21 +0200

    update

--- node.go
+++ node.go
@@ -58,18 +58,23 @@ func do(tlsConfig *tls.Config, handlers []*handler, address *net.UDPAddr, nodeAd
 		return err
 	}
 
+	type tReadData struct {
+		b       []byte
+		n       int
+		rAddr   *net.UDPAddr
+		cid     tID
+		mac     tID
+		nextMac tID
+		err     error
+	}
 	type tClient struct {
-		rAddr *net.UDPAddr
-		cids  []tID
+		rAddr     *net.UDPAddr
+		readData  []*tReadData
+		readIndex int
+		readed    int
 	}
 	var memory []*tClient
 
-	type tReadData struct {
-		b     []byte
-		n     int
-		rAddr *net.UDPAddr
-		err   error
-	}
 	cReadData := make(chan *tReadData, 512)
 	cFreeReadData := make(chan *tReadData, 512)
 	var i int
@@ -96,43 +101,58 @@ func do(tlsConfig *tls.Config, handlers []*handler, address *net.UDPAddr, nodeAd
 				if readData.err != nil {
 
 				}
-				cid = tID{}
-				client = nil
-				clientIndex = -1
-				iteration = 0
-				cClientIndex := make(chan int, lenMemory)
-				for i = 0; i < lenMemory; i++ {
-					go func(index int) {
-						j := len(memory[index].cids)
-						for j > 0 {
-							j--
-							if memory[index].cids[j].p1 == cid.p1 && memory[index].cids[j].p2 == cid.p2 &&
-								memory[index].cids[j].p3 == cid.p3 && memory[index].cids[j].p4 == cid.p4 {
-								cClientIndex <- index
-								return
+
+				if readData.b[0]&0x00000000 == 0 {
+					client = &tClient{}
+					b := sha256.Sum256(readData.b[1:readData.n])
+					readData.nextMac.p1 = uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+						uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
+					readData.nextMac.p2 = uint64(b[8]) | uint64(b[9])<<8 | uint64(b[10])<<16 | uint64(b[11])<<24 |
+						uint64(b[12])<<32 | uint64(b[13])<<40 | uint64(b[14])<<48 | uint64(b[15])<<56
+					readData.nextMac.p3 = uint64(b[16]) | uint64(b[17])<<8 | uint64(b[18])<<16 | uint64(b[19])<<24 |
+						uint64(b[20])<<32 | uint64(b[21])<<40 | uint64(b[22])<<48 | uint64(b[23])<<56
+					readData.nextMac.p4 = uint64(b[24]) | uint64(b[25])<<8 | uint64(b[26])<<16 | uint64(b[27])<<24 |
+						uint64(b[28])<<32 | uint64(b[29])<<40 | uint64(b[30])<<48 | uint64(b[31])<<56
+					client.readData = append(client.readData, readData)
+				} else {
+
+					client = nil
+					clientIndex = -1
+					iteration = 0
+					cClientIndex := make(chan int, lenMemory)
+					for i = 0; i < lenMemory; i++ {
+						go func(index int) {
+							j := len(memory[index].cids)
+							for j > 0 {
+								j--
+								if memory[index].cids[j].p1 == cid.p1 && memory[index].cids[j].p2 == cid.p2 &&
+									memory[index].cids[j].p3 == cid.p3 && memory[index].cids[j].p4 == cid.p4 {
+									cClientIndex <- index
+									return
+								}
 							}
-						}
-						cClientIndex <- -1
-					}(i)
-				}
-
-				for iteration < lenMemory {
-					select {
-					case i = <-cClientIndex:
-						if i >= 0 {
-							clientIndex = i
-						}
-						iteration++
+							cClientIndex <- -1
+						}(i)
 					}
+
+					for iteration < lenMemory {
+						select {
+						case i = <-cClientIndex:
+							if i >= 0 {
+								clientIndex = i
+							}
+							iteration++
+						}
+					}
+
+					if clientIndex < 0 {
+
+					}
+
+					client = memory[clientIndex]
+
+					cFreeReadData <- readData
 				}
-
-				if clientIndex < 0 {
-
-				}
-
-				client = memory[clientIndex]
-
-				cFreeReadData <- readData
 			}
 		}
 	}()
