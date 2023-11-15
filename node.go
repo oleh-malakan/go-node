b4ac360f6d5466f4a79cd54ee0dd4f06b19695fb
Oleh Malakan <olehdevt@gmail.com>
2023-11-15 11:35:41 +0200

    update

--- node.go
+++ node.go
@@ -15,12 +15,12 @@ func Handler(nodeID string, f func(query []byte, connection *Connection)) {
 	handlers = append(handlers, h)
 }
 
-func Do(tlsConfig *tls.Config, threads int, address *net.UDPAddr, nodeAddresses ...*net.UDPAddr) error {
+func Do(tlsConfig *tls.Config, address *net.UDPAddr, nodeAddresses ...*net.UDPAddr) error {
 	if tlsConfig == nil {
 		return newError("require tls config")
 	}
 
-	return do(handlers, tlsConfig, threads, address, nodeAddresses...)
+	return do(handlers, tlsConfig, address, nodeAddresses...)
 }
 
 type handler struct {
@@ -62,103 +62,96 @@ type tClient struct {
 	drop         bool
 }
 
-func do(handlers []*handler, tlsConfig *tls.Config, threads int,
-	address *net.UDPAddr, nodeAddresses ...*net.UDPAddr) error {
+func do(handlers []*handler, tlsConfig *tls.Config, address *net.UDPAddr, nodeAddresses ...*net.UDPAddr) error {
 	conn, err := net.ListenUDP("udp", address)
 	if err != nil {
 		return err
 	}
 
-	cFatal := make(chan error)
-
 	var memory *tClient
 	memoryLock := make(chan *struct{}, 1)
 
-	for i := 0; i < threads; i++ {
-		go func() {
-			for {
-				readData := &tReadData{
-					b: make([]byte, 1432),
+	for {
+		readData := &tReadData{
+			b: make([]byte, 1432),
+		}
+		readData.n, readData.rAddr, readData.err = conn.ReadFromUDP(readData.b)
+
+		go func(readData *tReadData) {
+			switch {
+			case readData.b[0]&0b00000000 == 0b00000000:
+				client := &tClient{
+					conn: tls.Server(&dataport{}, tlsConfig),
+					lock: make(chan *struct{}, 1),
 				}
-				readData.n, readData.rAddr, readData.err = conn.ReadFromUDP(readData.b)
+				readData.nextMac = sha256.Sum256(readData.b[1:readData.n])
 
-				switch {
-				case readData.b[0]&0b00000000 == 0b00000000:
-					client := &tClient{
-						conn: tls.Server(&dataport{}, tlsConfig),
-						lock: make(chan *struct{}, 1),
-					}
-					readData.nextMac = sha256.Sum256(readData.b[1:readData.n])
+				client.readData = readData
+				client.nextReadMac = readData.nextMac
+				client.drop = false
+				/*
+					go func() {
+						if err := client.conn.Handshake(); err != nil {
 
-					client.readData = readData
-					client.nextReadMac = readData.nextMac
-					client.drop = false
-					/*
-						go func() {
-							if err := client.conn.Handshake(); err != nil {
-
-							}
-						}()
-					*/
-
-					memoryLock <- nil
-					if memory != nil {
-						client.next = memory
-						memory = client
-					} else {
-						memory = client
-					}
-					<-memoryLock
-				case readData.b[0]&0b10000000 == 0b10000000 && memory != nil:
-					/*
-						readData.mac = readData.b[33:65]
-					*/
-					readData.nextMac = sha256.Sum256(readData.b[65:readData.n])
-					var client *tClient
-					memoryLock <- nil
-					if memory != nil {
-						client = memory
-						client.lock <- nil
-					}
-					<-memoryLock
-					for client != nil {
-						var next *tClient
-
-						if !client.drop {
-							w := client.writeData
-							m := client.lastWriteMac
-						LOOP:
-							if m[0] == readData.b[1] && m[1] == readData.b[2] && m[2] == readData.b[3] && m[3] == readData.b[4] &&
-								m[4] == readData.b[5] && m[5] == readData.b[6] && m[6] == readData.b[7] && m[7] == readData.b[8] &&
-								m[8] == readData.b[9] && m[9] == readData.b[10] && m[10] == readData.b[11] && m[11] == readData.b[12] &&
-								m[12] == readData.b[13] && m[13] == readData.b[14] && m[14] == readData.b[15] && m[15] == readData.b[16] {
-
-								//
-								//
-								//
-
-								goto FOUND
-							}
-							if w != nil && w.prev != nil {
-								w = w.prev
-								m = w.mac
-
-								goto LOOP
-							}
 						}
+					}()
+				*/
 
-						next = client.next
-						if next != nil {
-							next.lock <- nil
+				memoryLock <- nil
+				if memory != nil {
+					client.next = memory
+					memory = client
+				} else {
+					memory = client
+				}
+				<-memoryLock
+			case readData.b[0]&0b10000000 == 0b10000000 && memory != nil:
+				/*
+					readData.mac = readData.b[33:65]
+				*/
+				readData.nextMac = sha256.Sum256(readData.b[65:readData.n])
+				var client *tClient
+				memoryLock <- nil
+				if memory != nil {
+					client = memory
+					client.lock <- nil
+				}
+				<-memoryLock
+				for client != nil {
+					var next *tClient
+
+					if !client.drop {
+						w := client.writeData
+						m := client.lastWriteMac
+					LOOP:
+						if m[0] == readData.b[1] && m[1] == readData.b[2] && m[2] == readData.b[3] && m[3] == readData.b[4] &&
+							m[4] == readData.b[5] && m[5] == readData.b[6] && m[6] == readData.b[7] && m[7] == readData.b[8] &&
+							m[8] == readData.b[9] && m[9] == readData.b[10] && m[10] == readData.b[11] && m[11] == readData.b[12] &&
+							m[12] == readData.b[13] && m[13] == readData.b[14] && m[14] == readData.b[15] && m[15] == readData.b[16] {
+
+							//
+							//
+							//
+
+							goto FOUND
 						}
-					FOUND:
-						<-client.lock
-						client = next
+						if w != nil && w.prev != nil {
+							w = w.prev
+							m = w.mac
+
+							goto LOOP
+						}
 					}
+
+					next = client.next
+					if next != nil {
+						next.lock <- nil
+					}
+				FOUND:
+					<-client.lock
+					client = next
 				}
 			}
-		}()
+		}(readData)
 	}
-
-	return <-cFatal
 }
