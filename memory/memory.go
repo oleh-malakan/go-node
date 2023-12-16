7f747a15b039a0145d569ddadf9143f894354cf1
Oleh Malakan <olehdevt@gmail.com>
2023-12-16 15:41:14 +0200

    update

--- memory/memory.go
+++ memory/memory.go
@@ -6,23 +6,63 @@ const (
 )
 
 type indexArray[T any] struct {
-	array   []T
-	free    []int32
-	lenFree int32
+	array     []T
+	indexFree []int16
+	lenFree   int16
 }
 
-type continer[T any] struct {
-	cont *indexArray[T]
+func (a *indexArray[T]) put(v *T) int16 {
+	return 0
+}
+
+func (a *indexArray[T]) get(index int16) *T {
+	return nil
+}
+
+func (a *indexArray[T]) free(index int16) {
+
+}
+
+type orderContainer[T any] struct {
+	array []*T
 }
 
 type page[T any] struct {
-	page *continer[*indexArray[*T]]
+	p *orderContainer[*indexArray[T]]
+}
+
+func (p *page[T]) open() (int16, *indexArray[T]) {
+	return 0, nil
+}
+
+func (p *page[T]) get(index int16) *indexArray[T] {
+	return nil
+}
+
+func (p *page[T]) free(index int16) {
+
+}
+
+type memory[T any] struct {
+	m *orderContainer[*page[T]]
+}
+
+func (m *memory[T]) open() (int16, *page[T]) {
+	return 0, nil
+}
+
+func (m *memory[T]) get(index int16) *page[T] {
+	return nil
+}
+
+func (m *memory[T]) free(index int16) {
+
 }
 
 var EOF = &errorEOF{}
 
 type Memory[T any] struct {
-	memory *continer[*page[T]]
+	memory *memory[T]
 }
 
 func (m *Memory[T]) Put(v *T) (int32, error) {
