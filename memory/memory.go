52804857554db769bc865fe3d047e3ae03323498
Oleh Malakan <olehdevt@gmail.com>
2023-12-18 13:41:21 +0200

    update

--- memory/memory.go
+++ memory/memory.go
@@ -5,120 +5,178 @@ const (
 	depth = 1000000
 )
 
-type indexVector[T any] struct {
-	array     []*T
-	indexFree []int16
-	lenFree   int16
-}
-
-func (v *indexVector[T]) put(value *T) int16 {
-	if value != nil {
-		if v.lenFree > 0 {
-			v.lenFree--
-			index := v.indexFree[v.lenFree]
-			v.array[index] = value
-			return index
-		} else if index := int16(len(v.array)); index < cap {
-			v.array = append(v.array, value)
-			v.indexFree = append(v.indexFree, 0)
-			return index
-		}
-	}
-
-	return -1
-}
-
-func (v *indexVector[T]) get(index int16) *T {
-	if int(index) < len(v.array) {
-		return v.array[index]
-	}
-
-	return nil
-}
-
-func (v *indexVector[T]) free(index int16) {
-	if lenArray := len(v.array); int(index) < lenArray && v.array[index] != nil {
-		if lenArray--; int(index) < lenArray {
-			v.array[index] = nil
-			v.indexFree[v.lenFree] = index
-			v.lenFree++
-		} else {
-			v.array = v.array[:lenArray]
-			v.indexFree = v.indexFree[:lenArray]
-		}
-		if int(v.lenFree) == len(v.array) {
-			v.array = nil
-			v.indexFree = nil
-			v.lenFree = 0
-		}
-	}
-}
-
-func (v *indexVector[T]) len() int16 {
-	return int16(len(v.array))
-}
-
 type orderVector[T any] struct {
 	array     []*T
 	indexFree []int16
 }
 
-func (v *orderVector[T]) put(value *T) int16 {
+func (v *orderVector[T]) put(value *T) int {
 	if value != nil {
 		if len(v.indexFree) > 0 {
-
-		} else {
-
+			index := v.indexFree[0]
+			v.indexFree = v.indexFree[1:]
+			v.array[index] = value
+			return int(index)
+		} else if index := len(v.array); index < cap {
+			v.array = append(v.array, value)
+			return index
 		}
 	}
 
 	return -1
 }
 
-func (v *orderVector[T]) get(index int16) *T {
-	if int(index) < len(v.array) {
+func (v *orderVector[T]) get(index int) *T {
+	if index < len(v.array) {
 		return v.array[index]
 	}
 
 	return nil
 }
 
-func (v *orderVector[T]) free(index int16) {
+func (v *orderVector[T]) free(index int) {
+	if index < len(v.array) && v.array[index] != nil {
+		v.array[index] = nil
+		for i := 0; i < len(v.indexFree); i++ {
+			if index < int(v.indexFree[i]) {
+				temp := v.indexFree[i]
+				v.indexFree[i] = int16(index)
+				index = int(temp)
+			}
+		}
+		v.indexFree = append(v.indexFree, int16(index))
+
+		for len(v.indexFree) > 0 && int(v.indexFree[len(v.indexFree)-1]) == len(v.array)-1 {
+			v.array = v.array[:len(v.array)-1]
+			v.indexFree = v.indexFree[:len(v.indexFree)-1]
+		}
+	}
 }
 
-func (v *orderVector[T]) len() int16 {
-	return int16(len(v.array))
+func (v *orderVector[T]) len() int {
+	return len(v.array)
+}
+
+type column[T any] struct {
+	array     []*T
+	indexFree []int16
+	lenFree   int16
+}
+
+func (c *column[T]) put(value *T) int {
+	if value != nil {
+		if c.lenFree > 0 {
+			c.lenFree--
+			index := c.indexFree[c.lenFree]
+			c.array[index] = value
+			return int(index)
+		} else if index := len(c.array); index < cap {
+			c.array = append(c.array, value)
+			c.indexFree = append(c.indexFree, 0)
+			return index
+		}
+	}
+
+	return -1
+}
+
+func (c *column[T]) get(index int) *T {
+	if index < len(c.array) {
+		return c.array[index]
+	}
+
+	return nil
+}
+
+func (c *column[T]) free(index int) {
+	if lenArray := len(c.array); index < lenArray && c.array[index] != nil {
+		if lenArray--; int(index) < lenArray {
+			c.array[index] = nil
+			c.indexFree[c.lenFree] = int16(index)
+			c.lenFree++
+		} else {
+			c.array = c.array[:lenArray]
+			c.indexFree = c.indexFree[:lenArray]
+		}
+		if int(c.lenFree) == len(c.array) {
+			c.array = nil
+			c.indexFree = nil
+			c.lenFree = 0
+		}
+	}
+}
+
+func (c *column[T]) len() int {
+	return len(c.array)
 }
 
 type page[T any] struct {
-	p *orderVector[*indexVector[T]]
+	vector *orderVector[column[T]]
+	cursor int16
 }
 
-func (p *page[T]) open() (int16, *indexVector[T]) {
+func (p *page[T]) open() (int, *column[T]) {
+	var cursor int
+	if p.cursor > 0 {
+		cursor = int(p.cursor)
+	}
+
+	for i := cursor; i < p.vector.len(); i++ {
+		if column := p.vector.get(i); column != nil && column.len() < cap {
+			return i, column
+		}
+	}
+
+	if p.vector.len() < cap {
+		column := &column[T]{}
+		return p.vector.put(column), column
+	}
+
 	return 0, nil
 }
 
-func (p *page[T]) get(index int16) *indexVector[T] {
+func (p *page[T]) get(index int) *column[T] {
 	return nil
 }
 
-func (p *page[T]) free(index int16) {
+func (p *page[T]) free(index int) {
 
+}
+
+func (p *page[T]) len() int {
+	return p.vector.len()
 }
 
 type bank[T any] struct {
-	b *orderVector[*page[T]]
+	vector *orderVector[page[T]]
+	cursor int16
 }
 
-func (b *bank[T]) open() (int16, *page[T]) {
+func (b *bank[T]) open() (int, *page[T]) {
+	var cursor int
+	if b.cursor > 0 {
+		cursor = int(b.cursor)
+	}
+
+	for i := cursor; i < b.vector.len(); i++ {
+		if page := b.vector.get(i); page != nil && page.len() < cap {
+			return i, page
+		}
+	}
+
+	if b.vector.len() < cap {
+		page := &page[T]{}
+		return b.vector.put(page), page
+	}
+
 	return 0, nil
 }
 
-func (b *bank[T]) get(index int16) *page[T] {
+func (b *bank[T]) get(index int) *page[T] {
 	return nil
 }
 
-func (b *bank[T]) free(index int16) {
+func (b *bank[T]) free(index int) {
 
 }
 
@@ -128,8 +186,8 @@ type Memory[T any] struct {
 
 func (m *Memory[T]) Put(v *T) int32 {
 	if z, page := m.bank.open(); page != nil {
-		if y, array := page.open(); array != nil {
-			return int32(int(z)*depth + int(y)*cap + int(array.put(v)))
+		if y, column := page.open(); column != nil {
+			return int32(int(z)*depth + int(y)*cap + int(column.put(v)))
 		}
 	}
 
@@ -138,13 +196,13 @@ func (m *Memory[T]) Put(v *T) int32 {
 
 func (m *Memory[T]) Get(index int32) *T {
 	xy := int(index % depth)
-	x := int16(xy % cap)
-	y := int16(xy / cap)
-	z := int16(index / depth)
+	x := xy % cap
+	y := xy / cap
+	z := int(index / depth)
 
 	if page := m.bank.get(z); page != nil {
-		if array := page.get(y); array != nil {
-			return array.get(x)
+		if column := page.get(y); column != nil {
+			return column.get(x)
 		}
 	}
 
--- memory/memory_test.go
+++ memory/memory_test.go
@@ -5,7 +5,12 @@ import (
 	"testing"
 )
 
-func TestMemory(t *testing.T) {
+type test struct {
+	value int
+	index int
+}
+
+func TestMain(t *testing.T) {
 	index := 77871032
 	xy := index % depth
 	x := xy % cap
@@ -13,72 +18,126 @@ func TestMemory(t *testing.T) {
 	z := index / depth
 	index = int(z)*depth + int(y)*cap + int(x)
 	fmt.Println(x, y, z, index)
+}
 
-	type test struct {
-		value int
-		index int16
-	}
+func TestColumn(t *testing.T) {
+	c := &column[test]{}
 
-	a := &indexVector[test]{}
-
-	var err error
 	t0 := &test{value: 0}
-	t0.index = a.put(t0)
-	if err != nil {
-		fmt.Println(err)
-	}
+	t0.index = c.put(t0)
 
-	a.free(t0.index)
-	t0.index = a.put(t0)
+	c.free(t0.index)
+	t0.index = c.put(t0)
 
-	a.free(t0.index)
-	t0.index = a.put(t0)
+	c.free(t0.index)
+	t0.index = c.put(t0)
 
 	t1 := &test{value: 1}
-	t1.index = a.put(t1)
+	t1.index = c.put(t1)
 	t2 := &test{value: 2}
-	t2.index = a.put(t2)
+	t2.index = c.put(t2)
 
-	a.free(t1.index)
-	a.free(t1.index)
-	a.free(t0.index)
-	a.free(t2.index)
-	t0.index = a.put(t0)
-	t1.index = a.put(t1)
-	t2.index = a.put(t2)
+	c.free(t1.index)
+	c.free(t1.index)
+	c.free(t0.index)
+	c.free(t2.index)
+	t0.index = c.put(t0)
+	t1.index = c.put(t1)
+	t2.index = c.put(t2)
 
 	t3 := &test{value: 3}
-	t3.index = a.put(t3)
+	t3.index = c.put(t3)
 	t4 := &test{value: 4}
-	t4.index = a.put(t4)
+	t4.index = c.put(t4)
 	t5 := &test{value: 5}
-	t5.index = a.put(t5)
+	t5.index = c.put(t5)
 	t6 := &test{value: 6}
-	t6.index = a.put(t6)
+	t6.index = c.put(t6)
 	t7 := &test{value: 7}
-	t7.index = a.put(t7)
+	t7.index = c.put(t7)
 	t8 := &test{value: 8}
-	t8.index = a.put(t8)
+	t8.index = c.put(t8)
 	t9 := &test{value: 9}
-	t9.index = a.put(t9)
+	t9.index = c.put(t9)
 
-	a.free(t1.index)
-	t1.index = a.put(t1)
+	c.free(t1.index)
+	t1.index = c.put(t1)
 
-	a.free(t2.index)
-	a.free(t7.index)
-	a.free(t3.index)
+	c.free(t2.index)
+	c.free(t7.index)
+	c.free(t3.index)
 
-	a.free(t9.index)
-	a.free(t8.index)
+	c.free(t9.index)
+	c.free(t8.index)
 
-	t3.index = a.put(t3)
-	t2.index = a.put(t2)
-	t7.index = a.put(t7)
+	t3.index = c.put(t3)
+	t2.index = c.put(t2)
+	t7.index = c.put(t7)
 
-	tt := a.get(t5.index)
+	tt := c.get(t5.index)
 	fmt.Println(tt)
 
-	tt = a.get(55)
+	tt = c.get(55)
+	fmt.Println(tt)
+}
+
+func TestOrderVector(t *testing.T) {
+	v := *&orderVector[test]{}
+
+	t0 := &test{value: 0}
+	t0.index = v.put(t0)
+
+	v.free(t0.index)
+	t0.index = v.put(t0)
+
+	v.free(t0.index)
+	t0.index = v.put(t0)
+
+	t1 := &test{value: 1}
+	t1.index = v.put(t1)
+	t2 := &test{value: 2}
+	t2.index = v.put(t2)
+
+	v.free(t1.index)
+	v.free(t1.index)
+	v.free(t0.index)
+	v.free(t2.index)
+	t0.index = v.put(t0)
+	t1.index = v.put(t1)
+	t2.index = v.put(t2)
+
+	t3 := &test{value: 3}
+	t3.index = v.put(t3)
+	t4 := &test{value: 4}
+	t4.index = v.put(t4)
+	t5 := &test{value: 5}
+	t5.index = v.put(t5)
+	t6 := &test{value: 6}
+	t6.index = v.put(t6)
+	t7 := &test{value: 7}
+	t7.index = v.put(t7)
+	t8 := &test{value: 8}
+	t8.index = v.put(t8)
+	t9 := &test{value: 9}
+	t9.index = v.put(t9)
+
+	v.free(t1.index)
+	t1.index = v.put(t1)
+
+	v.free(t2.index)
+	v.free(t7.index)
+	v.free(t3.index)
+
+	v.free(t9.index)
+	v.free(t8.index)
+
+	t3.index = v.put(t3)
+	t2.index = v.put(t2)
+	t7.index = v.put(t7)
+
+	tt := v.get(t5.index)
+	fmt.Println(tt)
+
+	tt = v.get(55)
 	fmt.Println(tt)
 }
